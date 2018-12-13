b03c5884b5ffee78ff451704ce09fab188157631
danhy989 <danhy989@gmail.com>
2018-12-14 02:24:41 +0700

    push on server

--- app/src/main/java/company/danhy/clothesuit/activity/activity/activity/Thongtinkhachhang.java
+++ app/src/main/java/company/danhy/clothesuit/activity/activity/activity/Thongtinkhachhang.java
@@ -1,5 +1,6 @@
 package company.danhy.clothesuit.activity.activity.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +15,10 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -54,12 +59,57 @@ public class Thongtinkhachhang extends AppCompatActivity {
                 final String diachi=eddiachi.getText().toString().trim();
                 if(ten.length()>0&&sdt.length()>0&& email.length()>0&& diachi.length()>0){
                     RequestQueue requestQueue =Volley.newRequestQueue(getApplicationContext());
-                    StringRequest stringRequest =new StringRequest(Request.Method.POST, Server.duongThongTinKhachHang, new Response.Listener<String>() {
+                    StringRequest stringRequest =new StringRequest(Request.Method.POST, Server.duongDanThongTinKhachHang, new Response.Listener<String>() {
                         @Override
-                        public void onResponse(String response) {
-                            Log.d("madonhang",response);
+                        public void onResponse(final String madonhang) {
+                            Log.d("madonhang",madonhang);
+                            if(Integer.parseInt(madonhang)>0){
+                               RequestQueue Queue1 =Volley.newRequestQueue(getApplicationContext());
+                               StringRequest request=new StringRequest(Request.Method.POST, Server.duongDanChiTietDonHang, new Response.Listener<String>() {
+                                   @Override
+                                   public void onResponse(String response) {
+                                     if(response.equals("1")){
+                                         MainActivity.manggiohang.clear();
+                                         checkconnect.ShowToast_Short(getApplicationContext(),"Bạn đã thêm dữ liệu giỏ hàng thành công");
+                                         Intent intent =new Intent(getApplicationContext(),MainActivity.class);
+                                         startActivity(intent);
+                                         checkconnect.ShowToast_Short(getApplicationContext(),"Mời bạn tiếp tục mua hàng");
+                                     }else{
+                                         checkconnect.ShowToast_Short(getApplicationContext(),"Dữ liệu giỏ hàng của bạn đã bị lỗi");
+                                     }
+                                   }
+                               }, new Response.ErrorListener() {
+                                   @Override
+                                   public void onErrorResponse(VolleyError error) {
+
+                                   }
+                               }){
+                                   @Override
+                                   protected Map<String, String> getParams() throws AuthFailureError {
+                                       JSONArray jsonArray=new JSONArray();
+                                       for(int i=0;i<MainActivity.manggiohang.size();i++){
+                                           JSONObject jsonObject=new JSONObject();
+                                           try {
+                                               jsonObject.put("madonhang",madonhang);
+                                               jsonObject.put("masanpham",MainActivity.manggiohang.get(i).getIdsp());
+                                               jsonObject.put("tensanpham",MainActivity.manggiohang.get(i).getTensp());
+                                               jsonObject.put("giasanpham",MainActivity.manggiohang.get(i).getGiasp());
+                                               jsonObject.put("soluongsanpham",MainActivity.manggiohang.get(i).getSoluongsp());
+                                           } catch (JSONException e) {
+                                               e.printStackTrace();
+                                           }
+                                           jsonArray.put(jsonObject);
+                                       }
+                                       HashMap<String,String> hashMap=new HashMap<String,String>();
+                                       hashMap.put("json",jsonArray.toString());
+                                       return hashMap;
+                                   }
+                               };
+                               Queue1.add(request);
+                            }
                         }
-                    }, new Response.ErrorListener() {
+
+                        }, new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
 
@@ -77,7 +127,7 @@ public class Thongtinkhachhang extends AppCompatActivity {
                     };
                     requestQueue.add(stringRequest);
                 }else{
-                    checkconnect.ShowToast_Short(getApplicationContext(),"Bạn kiểm tra lại dữ liệu");
+                    checkconnect.ShowToast_Short(getApplicationContext(),"Bạn chưa nhập đủ thông tin");
                 }
             }
         });
--- app/src/main/java/company/danhy/clothesuit/activity/activity/ultil/Server.java
+++ app/src/main/java/company/danhy/clothesuit/activity/activity/ultil/Server.java
@@ -4,6 +4,9 @@ public class Server {
 
     //TriTue
     public static String localhost ="192.168.56.1:8080";
+   // public static String localhost ="192.168.51.2:8080";
+
+    //public static String localhost ="10.0.3.2:8080";
 
     //DuyAnh
    //public static String localhost ="172.17.24.247:80";
@@ -12,6 +15,7 @@ public class Server {
     public static String duongDanSanPhamMoiNhat="http://"+ localhost+ "/server/getsanphammoinhat.php";
     public static String duongDanGiay = "http://"+ localhost+ "/server/getsanpham.php?page=";
     public static String duongDanSanPhamFlashSale = "http://"+ localhost+ "/server/getsanphamflashsale.php";
-    public static String duongThongTinKhachHang ="http://"+ localhost +"/server/thongtinkhachhang.php";
+    public static String duongDanThongTinKhachHang ="http://"+ localhost +"/server/thongtinkhachhang.php";
+    public static String duongDanChiTietDonHang ="http://"+ localhost +"/server/chitietdonhang.php";
 
  }
